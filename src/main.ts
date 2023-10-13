ac996c44c3c7f5f72f02958c7cf0f03509148cee
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-13 18:19:16 +0800

    feat: 新增 hello pixi 文字到畫布上

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
+import { Application, Text } from 'pixi.js'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let app = new Application<HTMLCanvasElement>()
+document.body.appendChild(app.view)
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const text = new Text('Hello Pixi', {
+  fill: ['#ff0000', '#ff00ff']
+})
+
+app.stage.addChild(text)
