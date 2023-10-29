ff35c6aad9a000bf85b6ba2268cb58028f6a58ce
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-30 04:35:43 +0800

    test: 新增測試 ArrayUtils.removeItem()

--- src/test/ArrayUtils.test.ts
+++ src/test/ArrayUtils.test.ts
@@ -6,6 +6,16 @@ test('addUniqueItem: 在陣列裡加入一個不重覆的元素', () => {
   let array = ['a', 'b', 'c']
   expect(ArrayUtils.addUniqueItem(array, 'a')).toBe(false)
   expect(array.length).toBe(3)
+  
   expect(ArrayUtils.addUniqueItem(array, 'd')).toBe(true)
   expect(array.length).toBe(4)
+})
+
+test('removeItem: 把一個元素從陣列中移除', () => {
+  let array = ['a', 'b', 'c']
+  expect(ArrayUtils.removeItem(array, 'd')).toBe(false)
+  expect(array.length).toBe(3)
+
+  expect(ArrayUtils.removeItem(array, 'a')).toBe(true)
+  expect(array.length).toBe(2)
 })
