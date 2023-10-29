1e717153a5084487051b4171393bdc9ca2f35f1c
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-29 16:53:15 +0800

    test: 新增測試 ArrayUtils.addUniqueItem

---
+++ src/test/ArrayUtils.test.ts
@@ -0,0 +1,10 @@
+import { ArrayUtils } from "../lib/ArrayUtils";
+import { test, expect } from 'vitest'
+
+test('addUniqueItem: 在陣列裡加入一個不重覆的元素', () => {
+  let array = ['a', 'b', 'c']
+  expect(ArrayUtils.addUniqueItem(array, 'a')).toBe(false)
+  expect(array.length).toBe(3)
+  expect(ArrayUtils.addUniqueItem(array, 'd')).toBe(true)
+  expect(array.length).toBe(4)
+})
