60bc34e2a26cac381d8a1f67ce87f8bd82652ec0
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-30 01:25:44 +0800

    docs: 新增 vitest api 文件路徑

--- src/test/ArrayUtils.test.ts
+++ src/test/ArrayUtils.test.ts
@@ -1,5 +1,6 @@
 import { ArrayUtils } from "../lib/ArrayUtils";
 import { test, expect } from 'vitest'
+// 參考：https://cn.vitest.dev/api/#test
 
 test('addUniqueItem: 在陣列裡加入一個不重覆的元素', () => {
   let array = ['a', 'b', 'c']
