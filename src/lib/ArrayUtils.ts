b13480e7fbe192646bc3d1be05e1c7e6aadc69a4
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-30 04:28:55 +0800

    feat: 新增 ArrayUtils.removeItem method 把一個元素從陣列中移除

--- src/lib/ArrayUtils.ts
+++ src/lib/ArrayUtils.ts
@@ -9,11 +9,26 @@ export class ArrayUtils {
    * @param item 要加入的元素
    * @returns 若元素是唯一的且被加入則回傳 true
    */
-  static addUniqueItem(array: unknown[], item: unknown):boolean {
+  static addUniqueItem(array: unknown[], item: unknown): boolean {
     if (array.includes(item)) {
       return false
     }
     array.push(item)
+    return true
+  }
+
+  /**
+   * 把一個元素從陣列中移除
+   * @param array 目票陣列
+   * @param item 要移除的元素
+   * @returns 若成功移除則回傳 true
+   */
+  static removeItem(array: unknown[], item: unknown): boolean {
+    let index = array.indexOf(item)
+    if (index === -1) return false
+
+    // 使用 splice 在 index 的位置刪除 1 個元素
+    array.splice(index, 1)
     return true
   }
 }
