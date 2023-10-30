f2d389b91b2973c65f4b6b52a8d82f710e2092d7
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-30 14:50:28 +0800

    feat: 新增 ArrayUtils.getRandomItem 從陣列中隨機取得一個元素

--- src/lib/ArrayUtils.ts
+++ src/lib/ArrayUtils.ts
@@ -19,7 +19,7 @@ export class ArrayUtils {
 
   /**
    * 把一個元素從陣列中移除
-   * @param array 目票陣列
+   * @param array 目標陣列
    * @param item 要移除的元素
    * @returns 若成功移除則回傳 true
    */
@@ -30,5 +30,25 @@ export class ArrayUtils {
     // 使用 splice 在 index 的位置刪除 1 個元素
     array.splice(index, 1)
     return true
+  }
+
+  /**
+   * 從陣列中隨機取得一個元素
+   * @param array 目標陣列
+   * @param remove [可選] 要不要移除選到的元素
+   * @returns 隨機選擇的元素
+   */
+  static getRandomItem<T>(array: T[], remove?: boolean): T {
+    // 如果陣列長度為 0 丟出錯誤訊息
+    if (array.length === 0) throw new Error('無法從空陣列取得元素')
+  
+    let index = Math.floor(Math.random() * array.length)
+    let item = array[index]
+
+    // 如果需要則移除位於 index 的元素 
+    if (remove) {
+      array.splice(index, 1)
+    }
+    return item
   }
 }
