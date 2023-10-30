ba768454588994a4fe553c5a100cc0ca548f48b0
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-30 15:33:39 +0800

    test: 新增測試 ArrayUtils.getRandomItem()

--- src/test/ArrayUtils.test.ts
+++ src/test/ArrayUtils.test.ts
@@ -19,3 +19,21 @@ test('removeItem: 把一個元素從陣列中移除', () => {
   expect(ArrayUtils.removeItem(array, 'a')).toBe(true)
   expect(array.length).toBe(2)
 })
+
+test('getRandomItem: 從陣列中隨機取得一個元素', () => {
+  let array1 = ['a', 'b', 'c']
+  
+  // 從陣列中隨機取得一個元素
+  const randomItem1 = ArrayUtils.getRandomItem(array1)
+  expect(array1).toContain(randomItem1)
+  expect(array1.length).toBe(3)
+
+  // 從陣列中隨機取得一個元素並刪除該元素
+  const randomItem2 = ArrayUtils.getRandomItem(array1, true)
+  expect(array1).not.toContain(randomItem2)
+  expect(array1.length).toBe(2)
+
+  // 從空陣列中隨機取得一個元素會拋出錯誤
+  let array2: any[] = []
+  expect(() => ArrayUtils.getRandomItem(array2)).toThrowError('無法從空陣列取得元素')
+})
