0e432efeee2daf526595e921dce6f88a6aec4e8d
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-02 16:07:37 +0800

    test: 新增測試 StringUtils.capitalize() 將每個字的第一個英文字母變成大寫。注意：這個例子未考量到 @ # ! . 等符號開頭的單字

--- src/lib/StringUtils.ts
+++ src/lib/StringUtils.ts
@@ -4,7 +4,7 @@
 export class StringUtils {
   
   /**
-   * 將一句英文裡的每個字的第一個字母變成大寫
+   * 將每個字的第一個英文字母變成大寫
    * @param str 目標字串
    * @returns 字母大寫的字串
    */
@@ -12,6 +12,7 @@ export class StringUtils {
     return str.replace(/\b\w/g, (match, offset) => {
       const char = str[offset - 1];
       // 如果前一個字元是單引號則返回原字串 (避免 don't 變成 Don'T)
+      // 注意：這個例子未考量到 @ # ! . 等符號開頭的單字
       if (char === '\'') {
         return match
       }
---
+++ src/test/StringUtils.test.ts
@@ -0,0 +1,12 @@
+import { test, expect } from 'vitest'
+import { StringUtils } from '../lib/StringUtils'
+
+test('capitalize: 將每個字的第一個英文字母變成大寫', () => {
+  // 檢查一般句子
+  const output1 = StringUtils.capitalize('you are a good-person.')
+  expect(output1).toBe('You Are A Good-Person.')
+
+  // 檢查英文縮寫
+  const output2 = StringUtils.capitalize("i don't like rat.")
+  expect(output2).toBe("I Don't Like Rat.")
+})
