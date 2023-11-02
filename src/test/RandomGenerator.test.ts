7da37870ac75eb3c38e76a8a6c174115ebac1628
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-02 14:08:23 +0800

    test: 新增測試 RandomGenerator.next() 測試相同種子產生亂數的重覆性

--- src/test/RandomGenerator.test.ts
+++ src/test/RandomGenerator.test.ts
@@ -14,6 +14,20 @@ test('next: 輸出範圍是否介於 0 到 1', () => {
 })
 // 補充：同樣是範圍類的 nextBetween() 及 nextIntBetween() 就不再測試
 
+test('next: 測試相同種子產生亂數的重覆性', () => {
+  // 測 10 次，每次用一個新的種子建立 2 個亂數產生器，各產生 5 個亂數去比較是否相同。
+  let tries = 10
+  while (tries--) {
+    const seed = Math.round(Math.random() * 999996)
+    const rng1 = new RandomGenerator(seed)
+    const rng2 = new RandomGenerator(seed)
+    let length = 5
+    while (length--) {
+      expect(rng1.next()).toBe(rng2.next())
+    }
+  }
+})
+
 test('nextInt: 測試數字分佈是否平均', () => {
   const rng = new RandomGenerator(Date.now())
   // 定義骰子可擲出 0 到 9 共 10 個面
