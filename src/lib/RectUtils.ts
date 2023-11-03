68936a4cdb0bb937b998059cc772c24efec87fa5
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-03 11:36:10 +0800

    docs: 新增 RectUtils 說明

--- src/lib/RectUtils.ts
+++ src/lib/RectUtils.ts
@@ -1,4 +1,7 @@
 import { Rectangle } from 'pixi.js';
+/**
+ * 擴充 PIXI.Rectangle 的函式庫
+ */
 
 // 侵入 pixi.js 模組，在模組裡為 Rectangle 增加新的方法。
 declare module 'pixi.js' {
@@ -20,3 +23,12 @@ Rectangle.prototype.containsRect = function(other) {
     other.bottom <= this.bottom
   )
 }
+
+/**
+ * 註：其實 pixi 官方有為 Rectangle 和 Point 提供一些實用的數學方法
+ * 像上面寫的 containsRect()，官方的 @pixi/math-extras 已經有提供了
+ * 需安裝 npm install @pixi/math-extras
+ * 再 import '@pixi/math-extras'
+ * 並 import { Rectangle, Point } from '@pixi/core'
+ * 注意上面不是 import from 'pixi.js' 因為這是已經整包打包後的 pixi
+ */
