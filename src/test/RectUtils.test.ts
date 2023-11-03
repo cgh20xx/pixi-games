32b8b342be4230142dfb4b5c8d6d38096684dc9a
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-03 11:49:25 +0800

    test: 新增測試 Rectangle.containsRect() 檢查另一個矩形是否在這個矩形內

---
+++ src/test/RectUtils.test.ts
@@ -0,0 +1,13 @@
+import '../lib/RectUtils'
+import { describe, expect, test } from 'vitest';
+import { Rectangle } from 'pixi.js';
+
+describe('Rectangle', () => {
+  describe('containsRect', () => {
+    test('檢查另一個矩形是否在這個矩形內', () => {
+      const BigRect = new Rectangle(0, 0, 100, 100)
+      const smallRect = new Rectangle(10, 10, 20, 20)
+      expect(BigRect.containsRect(smallRect)).toBe(true)
+    })
+  })
+})
