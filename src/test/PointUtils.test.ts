e981f974b3772b69dbff8014665df09a07f342d7
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-06 01:43:23 +0800

    test: 新增測試 Observable 類別的向量長度

--- src/test/PointUtils.test.ts
+++ src/test/PointUtils.test.ts
@@ -1,12 +1,22 @@
 import '../lib/PointUtils'
 import { test, expect, describe } from 'vitest'
-import { Point } from 'pixi.js'
+import { ObservablePoint, Point } from 'pixi.js'
 
 describe('Point', () => {
   describe('length', () => {
     test('Point 類別的向量長度', () => {
         let point = new Point(3, 4)
         expect(point.length()).toBe(5)
+    })
+
+    test('Observable 類別的向量長度', () => {
+      let obPoint = new ObservablePoint(
+        () => {},
+        null,
+        13500,
+        12709
+      )
+      expect(obPoint.length()).toBe(18541)
     })
   })
 })
