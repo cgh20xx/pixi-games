adc9d42eee3a317b9898a6eca4ed93f4e2fdd5c1
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-06 16:54:47 +0800

    chore: 安裝 react eslint prettier 相關依賴及配置檔

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    // 以下有順序性的
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended' // npm install eslint-plugin-prettier --save-dev
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ]
+  }
+};
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+// vite-tsconfig-paths: https://github.com/aleclarson/vite-tsconfig-paths
+// 給 vite 有能力解析 imports using TypeScript's path mapping.
+// 讓 vite 看懂 tsconfig 裡的 baseUrl。
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()]
+});
