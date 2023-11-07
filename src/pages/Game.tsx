abcc442291b8567c12d114039f7f5970dd1411af
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-07 16:28:07 +0800

    feat: 修改 Game uesEffect cleanup function 內刪除 app.view

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -2,21 +2,23 @@ import { Application, Text } from 'pixi.js';
 import { useEffect, useRef } from 'react';
 
 const Game: React.FC = () => {
-  const pixiAppRef = useRef<HTMLDivElement>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const app = new Application<HTMLCanvasElement>();
     const text = new Text('Page Game', {
       fill: ['#ff0000', '#ff00ff']
     });
+    const divRefCurrent = divRef.current;
     app.stage.addChild(text);
-    pixiAppRef.current?.appendChild(app.view);
+    divRefCurrent?.appendChild(app.view);
 
     return () => {
+      divRefCurrent?.removeChild(app.view);
       app.destroy();
     };
   }, []);
-  return <div ref={pixiAppRef}></div>;
+  return <div ref={divRef}></div>;
 };
 
 export default Game;
