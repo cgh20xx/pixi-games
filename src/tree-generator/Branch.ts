9ce4aefc7fa653ff61442c59dbe1595ecdff2371
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-08 14:50:21 +0800

    feat: 新增 Branch.createChildren() 暫產生接在這根樹技尾端的子枝邏輯

--- src/tree-generator/Branch.ts
+++ src/tree-generator/Branch.ts
@@ -43,12 +43,31 @@ export class Branch {
    * 取得樹枝尾端的位置
    */
   getEndPosition(): Point {
-    const { options } = this;
+    const options = this.options;
     // 轉換生角度為弧度
     const radians = options.angle * DEG_TO_RAD;
     // 計算樹枝尾端的向量 (水平向量再旋轉)
     const vector = new Point(options.length).rotate(radians);
     // 尾端 = 起點 + 生長向量
     return options.position.add(vector);
+  }
+
+  /**
+   * 產生接在這根樹技尾端的子枝
+   */
+  createChildren(): void {
+    const options = this.options;
+    const treeOps = this.tree.options;
+    const rng = this.rng;
+    // 粗細比 1 大才會生出子枝
+    if (options.size > 1) {
+      // 亂數決定下一段是單枝還是要分兩枝
+    } else {
+      // 最細的樹枝尾端要長花
+    }
+
+    // 如果這個樹枝夠細，就讓它長葉子
+    if (options.size <= treeOps.leafBranchSize) {
+    }
   }
 }
