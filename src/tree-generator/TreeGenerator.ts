42289da0580496f2857c7a5f7b8603d2446f33e6
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-08 16:36:34 +0800

    feat: 修改 TreeGenerator.options 改為 tsdoc 注解

--- src/tree-generator/TreeGenerator.ts
+++ src/tree-generator/TreeGenerator.ts
@@ -1,23 +1,35 @@
 import { Application, Text } from 'pixi.js';
 
 export class TreeGenerator {
-  // 產生樹參數
+  /**
+   * 產生樹的配置選項。
+   */
   options = {
-    seed: 1, // 亂數種子 (同樣的種子會長出一樣的樹)
-    trunkSize: 10, // 主幹粗細
-    trunkLength: 120, // 主幹長度
-    branchRate: 0.8, // 分支機率
-    drawSpeed: 3, // 樹的成長速度
-    leafBranchSize: 4, // 這個粗細以下的樹技會長樹葉
-    branchColor: 0xffffff, // 樹枝顏色
-    leafColor: 0x00aa00, // 葉子顏色
-    flowerColor: 0xff6666 // 花的顏色
+    /** 亂數種子 (同樣的種子會長出一樣的樹) */
+    seed: 1,
+    /** 主幹粗細 */
+    trunkSize: 10,
+    /** 主幹長度 */
+    trunkLength: 120,
+    /** 分支機率 */
+    branchRate: 0.8,
+    /** 樹的成長速度 */
+    drawSpeed: 3,
+    /** 這個粗細以下的樹技會長樹葉 */
+    leafBranchSize: 4,
+    /** 樹枝顏色 */
+    branchColor: 0xffffff,
+    /** 葉子顏色 */
+    leafColor: 0x00aa00,
+    /** 花的顏色 */
+    flowerColor: 0xff6666
   };
 
   constructor(public app: Application) {
     const text = new Text('Tree', {
       fill: ['#ff0000', '#ff00ff']
     });
+    this.options.seed;
     app.stage.addChild(text);
   }
 }
