14ac7535124466758446ab73c2d744246af2f465
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-11 18:07:58 +0800

    feat: 新增 OptionsEditor.onButtonNext() 重新生長下一棵樹

--- src/tree-generator/OptionsEditor.ts
+++ src/tree-generator/OptionsEditor.ts
@@ -12,9 +12,23 @@ export class OptionsEditor {
     const options = generator.options;
     this.gui.add(options, 'seed', 1, 9999, 1);
     this.gui.add(this, 'onButtonGrow').name('重新生長');
+    this.gui.add(this, 'onButtonNext').name('重新生長下一顆樹');
   }
 
+  /**
+   * 重新生長
+   */
   onButtonGrow(): void {
     this.generator.newTree();
+  }
+
+  /**
+   * 重新生長下一棵樹
+   */
+  onButtonNext(): void {
+    this.generator.options.seed += 1;
+    this.generator.newTree();
+    // 因手動改變 seed，但 GUI 不知道，所以要呼叫 updateDisplay()
+    this.gui.updateDisplay();
   }
 }
