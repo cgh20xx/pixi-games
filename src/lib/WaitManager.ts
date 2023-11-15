a64b1c5e3940116bb3acaccf88e9398b587246c4
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-15 15:04:37 +0800

    feat: 新增 WaitManager.update()

--- src/lib/WaitManager.ts
+++ src/lib/WaitManager.ts
@@ -1,3 +1,5 @@
+import { Ticker } from 'pixi.js';
+
 class WaitProxy {
   constructor(
     /**
@@ -10,5 +12,54 @@ class WaitProxy {
 
   cancel() {
     this.reject('Wait canceled');
+  }
+}
+
+/**
+ * 等待管理員
+ * @class 處理所有等待者，每個等待者會在指定的時間到會觸發 Promise.resolve()
+ */
+export class WaitManager {
+  /**
+   * 儲存所有 WaitProxy 的陣列
+   */
+  private waits: WaitProxy[] = [];
+
+  /**
+   * 目前經過的時間 單位：frame
+   */
+  private now = 0;
+
+  constructor(public ticker: Ticker) {
+    ticker.add(this.update, this);
+  }
+
+  /**
+   * 取消偵聽 Ticker
+   */
+  destroy() {
+    this.ticker.remove(this.update, this);
+  }
+
+  /**
+   * 每次更新，累加經過時間，檢查所有等待 (WaitProxy) 是否有時間到，
+   * 有的話執行 resolve() 並移除等待陣列。
+   * @param dt deltaTime (frame)
+   */
+  private update(dt: number) {
+    this.now += dt;
+    // 持續 loop 直到等待陣列為空
+    while (this.waits.length) {
+      // 取得最前面的等待
+      const first = this.waits[0];
+      // 如果時間還沒到，離開 loop，不用再往下查
+      if (first.endTime > this.now) {
+        break;
+      }
+      // 把最前面的等待移除
+      this.waits.shift();
+      // 兌現等待的承諾
+      first.resolve();
+    }
   }
 }
