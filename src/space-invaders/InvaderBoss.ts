95e77e4496daaae7a057cd2a3dedb15dd04c8b08
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-28 14:45:38 +0800

    feat: 修改 InvaderBoss.attackUpdate 魔王攻擊更新函式

--- src/space-invaders/InvaderBoss.ts
+++ src/space-invaders/InvaderBoss.ts
@@ -1,6 +1,7 @@
 import { SpaceInvadersGame } from './SpaceInvadersGame';
 import { Invader } from './Invader';
 import { Point } from 'pixi.js';
+import { getStageSize } from 'lib/rwd-stage';
 
 /**
  * 外星魔王
@@ -19,6 +20,21 @@ export class InvaderBoss extends Invader {
   posInFlock = new Point();
 
   /**
+   * 移動速度
+   */
+  velocity = new Point();
+
+  /**
+   * 旋轉時間
+   */
+  rotateTime = 0;
+
+  /**
+   * 旋轉角速度
+   */
+  rotateSpeed = 0;
+
+  /**
    *
    * @param game SpaceInvadersGame instance
    * @param x 初始位罝 x
@@ -35,7 +51,30 @@ export class InvaderBoss extends Invader {
     this.goIdle();
   }
 
-  private attackUpdate() {}
+  private attackUpdate(dt: number) {
+    // 取得目前速率
+    const currentSpeed = this.velocity.length();
+    // 以 0.02 的速速度加快速率，最大值為 4
+    const speed = Math.min(4, currentSpeed + 0.02 * dt);
+    // 將速度的長度調整為新的速率
+    this.velocity.normalize(speed);
+
+    // 如果還有旋轉時間
+    if (this.rotateTime > 0) {
+      this.rotateTime -= dt;
+      // 將速度方向以 rotateSpeed 角速度旋轉
+      this.velocity.rotate(this.rotateSpeed * dt);
+    }
+
+    // 以速度的向量改變目前位置
+    this.x += this.velocity.x * dt;
+    this.y += this.velocity.y * dt;
+
+    if (this.y > getStageSize().height + this.height) {
+      // 如果 y 往下超出畫面下方，則進入 'back' mode
+      this.goBack();
+    }
+  }
 
   private backUpdate() {}
 
@@ -48,6 +87,8 @@ export class InvaderBoss extends Invader {
    * 跟著大軍移動
    */
   private async goIdle() {
+    // 進入下個模式前先把更新函式從 Ticker 中移除
+    this.removeUpdateFunctions();
     this.mode = 'idle';
     await this.game.wait(300);
     if (!this.destroyed) {
@@ -63,6 +104,23 @@ export class InvaderBoss extends Invader {
     this.removeUpdateFunctions();
     this.mode = 'attack';
     this.game.app.ticker.add(this.attackUpdate, this);
+    // 初始速度往上
+    this.velocity.set(0, -0.2);
+    // 旋轉時間: 210 ticks
+    this.rotateTime = 210;
+    // 旋轉角度速 (預設正值為順時針)
+    this.rotateSpeed = 0.02;
+    const cannon = this.game.cannon;
+    if (cannon && !cannon.dead) {
+      // 如果砲台在魔王右方
+      if (cannon.sprite.x > this.x) {
+        // 改為逆時針
+        this.rotateSpeed *= -1;
+      }
+    } else if (Math.random() < 0.5) {
+      // 如果砲台已不在，亂數決定要不要逆時針
+      this.rotateSpeed *= -1;
+    }
   }
 
   /**
