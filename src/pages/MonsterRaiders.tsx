56f944d243ef0a529c99b05242c51058e661a988
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-11-29 14:43:25 +0800

    feat: 新增 monster routes 及 MonsterRaiders page

---
+++ src/pages/MonsterRaiders.tsx
@@ -0,0 +1,31 @@
+import { Application } from 'pixi.js';
+import { initApp, setStageSize, refreshCanvasAndStage } from 'lib/rwd-stage';
+import { useEffect, useRef } from 'react';
+
+const MonsterRaiders: React.FC = () => {
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const app = new Application<HTMLCanvasElement>();
+    app.ticker.maxFPS = 60;
+    initApp(app);
+    setStageSize(640, 480); // setStageSize() 後要手動 refreshCanvasAndStage()
+    refreshCanvasAndStage(app);
+    // new SpaceInvadersGame(app);
+    const divRefCurrent = divRef.current;
+    divRefCurrent?.appendChild(app.view);
+    // 偵聽視窗的 resize 事件
+    function resizeHandler() {
+      refreshCanvasAndStage(app);
+    }
+    window.addEventListener('resize', resizeHandler);
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+      divRefCurrent?.removeChild(app.view);
+      app.destroy();
+    };
+  }, []);
+  return <div ref={divRef}></div>;
+};
+
+export default MonsterRaiders;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import type { RouteObject } from 'react-router-dom';
 import Tree from './pages/Tree';
 import SpaceInvaders from './pages/SpaceInvaders';
+import MonsterRaiders from 'pages/MonsterRaiders';
 import NotFound from './pages/NotFound';
 
 const routes: RouteObject[] = [
@@ -9,8 +10,16 @@ const routes: RouteObject[] = [
     element: <Tree />
   },
   {
+    path: '/tree',
+    element: <Tree />
+  },
+  {
     path: '/space',
     element: <SpaceInvaders />
+  },
+  {
+    path: '/monster',
+    element: <MonsterRaiders />
   },
   {
     path: '*',
