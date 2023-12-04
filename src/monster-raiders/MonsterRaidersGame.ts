cabfac012bb8045700a102ea4645bedf36497bac
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-12-05 00:04:20 +0800

    feat: 新增 MonsterRaidersGame.testCamera() 測試攝影機，使用鍵盤移動焦點。

--- src/monster-raiders/MonsterRaidersGame.ts
+++ src/monster-raiders/MonsterRaidersGame.ts
@@ -1,7 +1,10 @@
-import { Application, Container, PI_2, Point } from 'pixi.js';
+import { Application, Container, Graphics, PI_2, Point } from 'pixi.js';
 import { SpaceObject } from './SpaceObject';
 import { getStageSize } from 'lib/rwd-stage';
 import { Asteroid } from './Asteroid';
+import { Camera2D } from 'lib/camera/Camera2D';
+import { keyboardManager } from 'lib/keyboard/KeyboardManager';
+import { KeyCode } from 'lib/keyboard/KeyCode';
 
 /**
  * 怪戰掃蕩隊遊遊戲
@@ -25,6 +28,7 @@ export class MonsterRaidersGame extends Container {
     // 自動按照物件的 zIndex 重新排序。
     this.spaceRoot.sortableChildren = true;
     this.createInitAsteroids(4);
+    this.testCamera(); // 測試攝影機
   }
 
   /**
@@ -60,5 +64,42 @@ export class MonsterRaidersGame extends Container {
       // 放進小行星陣列
       this.objects.push(astroid);
     }
+  }
+
+  /**
+   * 測試攝影機
+   */
+  testCamera() {
+    // 製作攝影機的焦點，一個紅色的小圓
+    const focus = new Graphics();
+    focus.beginFill(0xff0000);
+    focus.drawCircle(0, 0, 10);
+    focus.endFill();
+    this.addChild(focus);
+    // 將焦點物件放在畫面中央
+    const stageSize = getStageSize();
+    focus.x = stageSize.width / 2;
+    focus.y = stageSize.height / 2;
+    // 建立攝影機
+    const camera = new Camera2D(this.app.ticker);
+    camera.width = stageSize.width;
+    camera.height = stageSize.height;
+    camera.focus = focus;
+    camera.gameRoot = this;
+    // 用鍵盤控制焦點物件
+    this.app.ticker.add(() => {
+      if (keyboardManager.isKeyDown(KeyCode.LEFT)) {
+        focus.x -= 1;
+      }
+      if (keyboardManager.isKeyDown(KeyCode.RIGHT)) {
+        focus.x += 1;
+      }
+      if (keyboardManager.isKeyDown(KeyCode.UP)) {
+        focus.y -= 1;
+      }
+      if (keyboardManager.isKeyDown(KeyCode.DOWN)) {
+        focus.y += 1;
+      }
+    });
   }
 }
