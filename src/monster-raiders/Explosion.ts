81e0fdd084e406bad307c73d7b513cb046c76762
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-12-10 17:58:04 +0800

    feat: 新增 Explosion.playAndDestroy() 播放爆炸動畫，並在結束時自我銷毀

--- src/monster-raiders/Explosion.ts
+++ src/monster-raiders/Explosion.ts
@@ -1,4 +1,5 @@
 import { AnimatedSprite, Container } from 'pixi.js';
+import { SpaceObject } from './SpaceObject';
 
 /**
  * 爆炸特效
@@ -22,5 +23,22 @@ export class Explosion extends Container {
     this.animation.loop = false;
     // 加進爆炸的繪圖容器
     this.addChild(this.animation);
+  }
+
+  /**
+   * 播放爆炸動畫，並在結束時自我銷毀
+   * @param target 太空物件
+   */
+  playAndDestroy(target: SpaceObject) {
+    // 加入遊戲容器
+    target.game.spaceRoot.addChild(this);
+    // 將位置移到發生爆炸的太空物件
+    this.position.copyFrom(target.position);
+    // 依爆炸物件的碰撞半徑調整特效大小
+    this.animation.scale.set(target.hitRadius / 32);
+    // 播放動畫
+    this.animation.play();
+    // 播完動畫時銷毀
+    this.animation.onComplete = () => this.destroy();
   }
 }
