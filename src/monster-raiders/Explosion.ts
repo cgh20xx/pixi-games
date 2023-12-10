86a6ef8cc5fe220a8b3a1250831e4c0494f0a4b7
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-12-10 18:10:16 +0800

    feat: 新增 Explosion.getTextures() 取得爆炸動畫所有材質

--- src/monster-raiders/Explosion.ts
+++ src/monster-raiders/Explosion.ts
@@ -1,5 +1,12 @@
-import { AnimatedSprite, Container } from 'pixi.js';
+import {
+  AnimatedSprite,
+  BaseTexture,
+  Container,
+  Rectangle,
+  Texture
+} from 'pixi.js';
 import { SpaceObject } from './SpaceObject';
+import explosionImg from 'images/explosion-spritesheet.png';
 
 /**
  * 爆炸特效
@@ -16,7 +23,7 @@ export class Explosion extends Container {
     // zIndex 要大於其它太空物件
     this.zIndex = 10;
     // 建立爆炸動畫
-    this.animation = new AnimatedSprite([]);
+    this.animation = new AnimatedSprite(getTextures());
     // 調整動畫的軸心至圖片中央
     this.animation.anchor.set(0.5);
     // 不重覆播放動畫
@@ -41,4 +48,43 @@ export class Explosion extends Container {
     // 播完動畫時銷毀
     this.animation.onComplete = () => this.destroy();
   }
+}
+
+/**
+ * 爆炸動畫中每一幀的長寬尺寸
+ */
+const frameSize = {
+  width: 128,
+  height: 124
+};
+
+/**
+ * 爆炸動畫材質陣列的快取
+ */
+const texturesCache: Texture[] = [];
+
+/**
+ * 取得爆炸動畫所有材質
+ */
+function getTextures(): Texture[] {
+  // 若快取是空的，就要建立材質陣列的快取
+  if (texturesCache.length === 0) {
+    // 載入基底材質
+    const baseTexture = BaseTexture.from(explosionImg);
+    // 組建 48 張不同部位的材質
+    for (let i = 0; i < 48; i++) {
+      const col = i % 8;
+      const rol = Math.floor(i / 8);
+      const frame = new Rectangle(
+        col * frameSize.width,
+        rol * frameSize.height,
+        frameSize.width,
+        frameSize.height
+      );
+      const texture = new Texture(baseTexture, frame);
+      texturesCache.push(texture);
+    }
+  }
+  // 回傳材質陣列的快取
+  return texturesCache;
 }
