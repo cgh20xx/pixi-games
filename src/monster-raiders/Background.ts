7fa5f2ad06bd49933997b9cf032710fbb9d658d1
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-12-11 14:54:59 +0800

    feat: 修改 Background 新增半透明星空背景，調整不同背景的移動速度營造視差。

--- src/monster-raiders/Background.ts
+++ src/monster-raiders/Background.ts
@@ -1,6 +1,7 @@
 import { Container, Texture, TilingSprite } from 'pixi.js';
 import { MonsterRaidersGame } from './MonsterRaidersGame';
 import starrySpaceImg from 'images/starry-space.png';
+import starsImg from 'images/stars.png';
 import { getStageSize } from 'lib/rwd-stage';
 
 /**
@@ -8,17 +9,22 @@ import { getStageSize } from 'lib/rwd-stage';
  */
 export class Background extends Container {
   /**
-   * 星空 TilingSprite
+   * 黑底星空 TilingSprite
    * @see https://pixijs.com/examples/sprite/tiling-sprite
    */
   starrySprite: TilingSprite;
 
+  /**
+   * 半透明星空 TilingSprite
+   * @see https://pixijs.com/examples/sprite/tiling-sprite
+   */
+  starsSprite: TilingSprite;
+
   constructor(public game: MonsterRaidersGame) {
     super();
     // 黑底星空
-    const texture = Texture.from(starrySpaceImg);
     this.starrySprite = new TilingSprite(
-      texture,
+      Texture.from(starrySpaceImg),
       getStageSize().width,
       getStageSize().height
     );
@@ -26,12 +32,20 @@ export class Background extends Container {
     this.starrySprite.tileScale.set(0.5);
     // 將 Background 加入遊戲容器的最底層
     game.addChildAt(this, 0);
+    // 半透明星空
+    this.starsSprite = new TilingSprite(
+      Texture.from(starsImg),
+      getStageSize().width,
+      getStageSize().height
+    );
+    this.addChild(this.starsSprite);
+    this.starsSprite.tileScale.set(0.8);
     // 開始更新循環
     this.game.app.ticker.add(this.update, this);
   }
 
   /**
-   * 銷毀星空背景物件
+   * 銷毀背景容器
    * @override
    */
   destroy() {
@@ -43,11 +57,18 @@ export class Background extends Container {
    * 更新函式
    */
   update() {
-    // 讓背景跟據攝影機的位置以一定比例去平移
+    // 讓背景根據攝影機的位置以一定比例去平移
     const camera = this.game.camera;
     // 以 0.2 的比例平移最下層黑底星空的 tile 原點
-    const shiftRate = 0.2;
+    let shiftRate = 0.2;
     this.starrySprite.tilePosition.set(
+      -camera.position.x * shiftRate,
+      -camera.position.y * shiftRate
+    );
+
+    // 以 0.25 的比例平移半透明星空的 tile 原點，營造視差效果
+    shiftRate = 0.25;
+    this.starsSprite.tilePosition.set(
       -camera.position.x * shiftRate,
       -camera.position.y * shiftRate
     );
