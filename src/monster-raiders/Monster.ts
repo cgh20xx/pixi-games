9340f11ac5d7897a92b1947fdca137a00dd9d7df
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-12-13 14:59:25 +0800

    feat: 新增 Monster.update() 怪獸自動追蹤轉向功能

--- src/monster-raiders/Monster.ts
+++ src/monster-raiders/Monster.ts
@@ -1,6 +1,7 @@
 import { PI_2, Sprite } from 'pixi.js';
 import { SpaceObject, SpaceObjectType } from './SpaceObject';
 import monsterImg from 'images/space-monster.png';
+import { MathUtils } from 'lib/MathUtils';
 
 export class Monster extends SpaceObject {
   // 不用覆寫 constructor
@@ -11,6 +12,11 @@ export class Monster extends SpaceObject {
   get type(): SpaceObjectType {
     return 'monster';
   }
+
+  /**
+   * 還要追著玩家跑多久 (單位：偵)
+   */
+  followDuration = 200;
 
   /**
    * 覆寫 SpaceObject.init()
@@ -34,5 +40,43 @@ export class Monster extends SpaceObject {
     this.hitRadius = 20;
     // 並畫出碰撞圓 (測試用)
     this.drawHitCircle();
+  }
+
+  /**
+   * 更新函式
+   * @param dt 經過時間
+   * @override
+   */
+  update(dt: number) {
+    // 先找到玩家戰機
+    const fighter = this.game.objects.find(obj => {
+      return obj.type === 'fighter';
+    });
+    if (fighter && this.followDuration > 0) {
+      // 縮短追蹤玩家的時間
+      this.followDuration -= dt;
+      // // 計算兩物之間的向量
+      const vector = fighter.position.sub(this.position);
+      // // 計算戰機對於我的方向 (弧度)
+      const radians = Math.atan2(vector.y, vector.x);
+      // // 計算和我目前的方向差
+      let radDiff = radians - this.rotation;
+      radDiff = MathUtils.normalizeRadians(radDiff);
+      // // 依弧度差來轉向
+      const rotateSpeed = 0.025;
+      if (radDiff > 0) {
+        // 逆時針轉
+        const rad = Math.min(radDiff, rotateSpeed * dt);
+        this.rotation += rad;
+      } else {
+        // 順時針轉
+        const rad = Math.max(radDiff, -rotateSpeed * dt);
+        this.rotation += rad;
+      }
+      // // 依新的方向調整飛行速度的向量
+      this.velocity.set(2, 0);
+      this.velocity.rotate(this.rotation);
+    }
+    super.update(dt);
   }
 }
