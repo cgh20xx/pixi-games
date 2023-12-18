05738ee97754791073764cd6c721534d5590f128
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-12-18 10:57:05 +0800

    feat: 新增 MonsterRaidersGameOver.drawBackground() 畫對話框的背景及罝中

--- src/monster-raiders/MonsterRaidersGameOver.ts
+++ src/monster-raiders/MonsterRaidersGameOver.ts
@@ -1,5 +1,6 @@
-import { Container } from 'pixi.js';
+import { Container, Graphics } from 'pixi.js';
 import { MonsterRaidersGame } from './MonsterRaidersGame';
+import { getStageSize } from 'lib/rwd-stage';
 
 /**
  * 遊戲結束畫面
@@ -9,6 +10,26 @@ export class MonsterRaidersGameOver extends Container {
   constructor(public game: MonsterRaidersGame) {
     super();
     console.log('MonsterRaidersGameOver');
+    // 畫對話框的背景
+    this.drawBackground(480, 240);
     game.addChild(this);
+    // 對話框置中
+    this.position.set(
+      (getStageSize().width - this.width) / 2,
+      (getStageSize().height - this.height) / 2
+    );
+  }
+
+  /**
+   * 畫對話框的背景
+   * @param width 寬
+   * @param height 高
+   */
+  drawBackground(width: number, height: number) {
+    const graphics = new Graphics();
+    graphics.beginFill(0xffffff, 0.5);
+    graphics.drawRoundedRect(0, 0, width, height, 10);
+    graphics.endFill();
+    this.addChild(graphics);
   }
 }
