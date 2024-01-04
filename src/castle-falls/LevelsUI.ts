e347e9f67a807fd0eb934808ea0cfe6c45536484
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-01-04 21:46:26 +0800

    feat: 新增 LevelsUI.createLevelButton() 方法 (未實作)

--- src/castle-falls/LevelsUI.ts
+++ src/castle-falls/LevelsUI.ts
@@ -9,5 +9,20 @@ export class LevelsUI extends Container {
     const bg = Sprite.from(castleBgImg);
     bg.scale.set(0.5);
     this.addChild(bg);
+    // 建立選關按鈕
+    const maxLevel = 3;
+    for (let lv = 0; lv < maxLevel; lv++) {
+      this.createLevelButton(lv);
+    }
+  }
+
+  /**
+   * 建立關卡按鈕
+   * @param level 關卡
+   */
+  private createLevelButton(level: number) {
+    // 按鈕標籤
+    const label = `第${level}關`;
+    // 建立按鈕
   }
 }
