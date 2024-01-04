b4e6d77eebc2633b0ab8ef024e024b3e3dec7f24
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-01-04 22:21:06 +0800

    feat: 修改 LevelsUI.createLevelButton() 將按鈕背景顯示在畫面上

--- src/castle-falls/LevelsUI.ts
+++ src/castle-falls/LevelsUI.ts
@@ -1,6 +1,7 @@
 import { Container, Sprite } from 'pixi.js';
 import { CastleFalls } from './CastleFalls';
 import castleBgImg from 'images/castle-bg.jpg';
+import { PixiButton } from './PixiButton';
 
 export class LevelsUI extends Container {
   constructor(public gameApp: CastleFalls) {
@@ -20,9 +21,33 @@ export class LevelsUI extends Container {
    * 建立關卡按鈕
    * @param level 關卡
    */
-  private createLevelButton(level: number) {
+  private createLevelButton(level: number): PixiButton {
     // 按鈕標籤
     const label = `第${level}關`;
     // 建立按鈕
+    const button = new PixiButton({
+      width: 240,
+      height: 36,
+      cornerRadius: 8,
+      label: label,
+      labelSize: 24,
+      labelColor: {
+        default: 0xffffff,
+        hover: 0x333333,
+        active: 0xffffff
+      },
+      backgroundColor: {
+        default: 0x333333,
+        hover: 0xffffff,
+        active: 0xaa0000
+      },
+      onClick: () => {
+        // Todo: 先把介面毀滅，再開始遊戲
+      }
+    });
+    this.addChild(button);
+    button.x = 50;
+    button.y = 40 + (level - 1) * 50;
+    return button;
   }
 }
