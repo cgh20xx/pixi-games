8f225aa56b4c4e2f5cbb3f24569d7c68b98ee81e
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-01-13 16:26:36 +0800

    feat: 新增 Slingshot.createSprites() 建立彈弓的精靈圖

--- src/castle-falls/Slingshot.ts
+++ src/castle-falls/Slingshot.ts
@@ -1,5 +1,8 @@
+import { Sprite } from 'pixi.js';
 import { CastleFallsGame } from './CastleFallsGame';
 import { ICFSlingshot } from './CastleFallsLevelData';
+import slingshotImg from 'images/slingshot.png';
+import slingshotFrontImg from 'images/slingshot_front.png';
 
 /**
  * 彈弓
@@ -10,8 +13,26 @@ export class Slingshot {
     data: ICFSlingshot
   ) {
     // 建立彈弓的精靈圖
+    this.createSprites(data);
     // 建立一顆石頭
     // 建立橡皮筋
     // 建立滑鼠約束，讓玩家可以用滑鼠拉石頭
+  }
+
+  /**
+   * 建立彈弓的精靈圖
+   * @param data 建立彈弓的資料
+   */
+  private createSprites(data: ICFSlingshot): void {
+    // 彈弓主體
+    const backSprite = Sprite.from(slingshotImg);
+    backSprite.zIndex = 0;
+    backSprite.position.set(data.x - 35, data.y - 15);
+    this.game.addChild(backSprite);
+    // 能遮住石塊的木段
+    const frontSprite = Sprite.from(slingshotFrontImg);
+    frontSprite.zIndex = 10; // 石頭的 zIndex = 6
+    frontSprite.position.copyFrom(backSprite);
+    this.game.addChild(frontSprite);
   }
 }
