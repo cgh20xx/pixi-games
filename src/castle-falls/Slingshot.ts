63cfb27c20d2e48f24b6fcffdf7c49d0279d0a35
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-01-13 16:44:38 +0800

    feat: 新增 Slingshot.createRock() 建立發射用的石頭

--- src/castle-falls/Slingshot.ts
+++ src/castle-falls/Slingshot.ts
@@ -3,6 +3,7 @@ import { CastleFallsGame } from './CastleFallsGame';
 import { ICFSlingshot } from './CastleFallsLevelData';
 import slingshotImg from 'images/slingshot.png';
 import slingshotFrontImg from 'images/slingshot_front.png';
+import { Body } from 'matter-js';
 
 /**
  * 彈弓
@@ -15,6 +16,7 @@ export class Slingshot {
     // 建立彈弓的精靈圖
     this.createSprites(data);
     // 建立一顆石頭
+    this.createRock(data);
     // 建立橡皮筋
     // 建立滑鼠約束，讓玩家可以用滑鼠拉石頭
   }
@@ -34,5 +36,22 @@ export class Slingshot {
     frontSprite.zIndex = 10; // 石頭的 zIndex = 6
     frontSprite.position.copyFrom(backSprite);
     this.game.addChild(frontSprite);
+  }
+
+  /**
+   * 建立發射用的石頭
+   * @param data 建立彈弓的資料
+   * @returns Matter.Body
+   */
+  private createRock(data: ICFSlingshot): Body {
+    const object = this.game.createMatterObject({
+      type: 'rock',
+      x: data.x,
+      y: data.y + 1, // 石頭位置不要和橡皮筋的其中一端重疊，避免橡皮筋約束出問題，故 +1。
+      circle: {
+        radius: 15
+      }
+    });
+    return object.body;
   }
 }
