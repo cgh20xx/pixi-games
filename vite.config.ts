b899860a208a18924300ba9122157975b9e2c480
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-02-04 20:39:10 +0800

    feat: 修改 vite.config.ts 使 dev server可使用內部 ip

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // build 出的資源改為相對路徑
+  server: {
+    host: '0.0.0.0'
+  },
   plugins: [react(), tsconfigPaths()]
 });
